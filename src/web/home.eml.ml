bb09e041f70f2e8708c71ef13e673eb250eec153
Rawley Fowler <rawleyfowler@gmail.com>
2022-10-05 17:27:49 -0600

    add module type

---
+++ src/web/home.eml.ml
@@ -0,0 +1,5 @@
+let render () =
+  <h1>Dev-Pro Logging</h1>
+  <div>
+    Logs will go here
+  </div>
--- src/web/web.ml
+++ src/web/web.ml
@@ -1,15 +1,5 @@
 module Database = Db
 module Log = Db.Log
-let home = 
-  <html>
-  <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-  </head>
-  <body>
-    <h1>Home Page</h1>
-  </body>
-  </html>
 
 let save log = 
   Database.insert_log ~log:log
@@ -20,18 +10,6 @@ let add_log req =
   match log with
   | Ok l -> Db.insert_log ~log:l; Dream.respond ~code:201 ""
   | Error _ -> Dream.respond ~code:401 ""
-(* let handle_add request =
-  let%lwt body = Dream.body request in
-    let log = log_of_yojson @@ Yojson.Safe.from_string body in
-    match log with
-    | Ok l -> save l
-    | Error _ -> Dream.respond ~code:500 "" *)
-
-(* let get f param =
-  let%lwt logs = f param in
-  match logs with
-  | Ok lst -> `List lst |> Yojson.Safe.to_string |> Dream.json
-  | Error _ -> Dream.respond ~code:404 "" *)
 
 let get_all () =
   let logs = Database.find_all_logs () in
@@ -41,12 +19,18 @@ let get_by_app app =
   let logs = Database.find_by_app ~app:app in
     `List (List.map Db.Log.to_yojson logs) |> Yojson.Safe.to_string |> Dream.json
 
+module type Render = sig val render : unit -> string end
+
+let render (module R : Render) = 
+  R.render |> Template.render |> Dream.html
+
 let placeholder = fun _ -> Dream.html "Hello World"
+
 let run () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router [
-    Dream.get "/" (fun _ -> Dream.html home);
+    Dream.get "/" (fun _ -> render (module Home));
     Dream.scope "/logs" [] [
       Dream.get "/" (fun _ -> get_all ());
       Dream.get "/app/:app" (fun request ->
